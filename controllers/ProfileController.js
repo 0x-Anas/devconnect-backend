de2233df12614b710a73ce655e7edb0157d13b66
0x-Anas <muhammedanasoff@gmail.com>
2025-05-13 21:55:33 +0530

    updated backend profile controller with dp uplaod

---
+++ controllers/ProfileController.js
@@ -0,0 +1,91 @@
+const Profile=require('../models/Profile');
+const User=require('../models/Users')
+
+//to fetch users profile
+const getMyProfile=async(req,res)=>{
+    try{
+        const profile=await Profile.findOne({user:req.userId}).populate('user', 'username profilePic');
+        if(!profile){
+            res.status(404).json({message:'profile not found'})
+        }
+        res.status(200).json(profile)
+    }
+    catch(error){
+        console.error('error',error)
+        res.status(500).json({message:'server error while fetching profile'})
+    }
+}
+
+//create and updating profile
+const createOrUpdateProfile=async(req,res)=>{
+    const { bio, skills, github, linkedin, website, location } = req.body;
+
+    const profileData={
+        user: req.userId,
+        bio,
+        skills: skills?.split(',').map(s => s.trim()),
+        github,
+        linkedin,
+        website,
+        location
+    }
+    try{
+
+        let profile = await Profile.findOne({ user: req.userId });
+        if (profile) {
+            profile = await Profile.findOneAndUpdate(
+              { user: req.userId },
+              { $set: profileData },
+              { new: true }
+            );
+            return res.json(profile);
+          }
+
+        profile=new Profile(profileData)
+        await profile.save();
+        return res.status(201).json(profile)
+}catch (error) {
+    console.error('Error saving profile:', error.message);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+//for viewing users profile
+const UsersProfile=async(req,res)=>{
+    try{
+        const {username}=req.params;
+
+        const user=await User.findOne({username}).select('-password');//??
+
+        if(!user){
+            return res.status(404).json({message:'user not found'});
+
+        }
+        res.status(200).json(user);
+    }catch(err){
+        console.error('Error in GET /users/:username:', err)
+        res.status(500).json({message:'server error',err})
+    }
+};
+const uploadProfilePicture=async(req,res)=>{
+    try{
+        const user=await User.findByIdAndUpdate(
+            req.userId,
+            {profilePicture:`/uploads/${req.file.filename}`},
+            {new:true}
+        );
+        res.status(200).json({
+            message:'Profile picture updated',
+            profilePicture:user.profilePicture
+        });
+    }catch(err){
+        console.error('error upload profile picture:',err)
+        res.status(500).json({ message: 'Server error during profile picture upload',err})
+    }
+}
+module.exports={
+    getMyProfile,
+    createOrUpdateProfile,
+    UsersProfile,
+    uploadProfilePicture
+}
